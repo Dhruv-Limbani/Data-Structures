36093a69a61c40dfdab3ef6092b102e0f1da6b61
Dhruv <informdhruv@gmail.com>
2024-02-14 19:07:29 +0530

    removed declaration

--- logical thinking/patterns.cpp
+++ logical thinking/patterns.cpp
@@ -1,60 +1,6 @@
 #include<iostream>
 using namespace std;
 
-void pattern1(int n);
-void pattern2(int n);
-void pattern3(int n);
-void pattern4(int n);
-void pattern5(int n);
-void pattern6(int n);
-void pattern7(int n);
-void pattern8(int n);
-void pattern9(int n);
-void pattern10(int n);
-int main()
-{
-    int n, choice;
-    cout<<"Enter pattern number and length: ";
-    cin>>choice>>n;
-    switch(choice){
-        case 1:
-            pattern1(n);
-            break;
-        case 2:
-            pattern2(n);
-            break;
-        case 3:
-            pattern3(n);
-            break;
-        case 4:
-            pattern4(n);
-            break;
-        case 5:
-            pattern5(n);
-            break;
-        case 6:
-            pattern6(n);
-            break;
-        case 7:
-            pattern7(n);
-            break;
-        case 8:
-            pattern8(n);
-            break;
-        case 9:
-            pattern9(n);
-            break;
-        case 10:
-            pattern10(n);
-            break;
-        default:
-            cout<<"Incorrect Choice! Choose between 1 to 10";
-    }
-        
-    //pattern1(n);
-    return 0;
-}
-
 void pattern1(int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -210,3 +156,49 @@ void pattern10(int n)
 // **
 // *
 }
+
+int main()
+{
+    int n, choice;
+    cout<<"Enter pattern number and length: ";
+    cin>>choice>>n;
+    switch(choice){
+        case 1:
+            pattern1(n);
+            break;
+        case 2:
+            pattern2(n);
+            break;
+        case 3:
+            pattern3(n);
+            break;
+        case 4:
+            pattern4(n);
+            break;
+        case 5:
+            pattern5(n);
+            break;
+        case 6:
+            pattern6(n);
+            break;
+        case 7:
+            pattern7(n);
+            break;
+        case 8:
+            pattern8(n);
+            break;
+        case 9:
+            pattern9(n);
+            break;
+        case 10:
+            pattern10(n);
+            break;
+        default:
+            cout<<"Incorrect Choice! Choose between 1 to 10";
+    }
+        
+    //pattern1(n);
+    return 0;
+}
+
+

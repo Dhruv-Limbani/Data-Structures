0c1cbc9d72e27f62cafd670edca37fe23fccd2fd
Dhruv <informdhruv@gmail.com>
2024-11-26 14:48:43 -0500

    dp medium

---
+++ DP/DP_on_subsequences/longest_increasing_subsequence.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// dp
+int lengthOfLIS(vector<int>& nums, vector<vector<int>> &dp, int ind, int prev_ind){
+    if(ind>=nums.size()) return 0;
+    if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
+    int not_take = lengthOfLIS(nums, dp, ind+1,prev_ind), take = 0;
+    if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
+        take = 1 + lengthOfLIS(nums, dp, ind+1,ind);
+    }
+    return dp[ind][prev_ind+1] = max(take, not_take);
+}
+
+// binary search
+void find_replacement_and_replace(vector<int> &ans, int x){
+    int low=0, high=ans.size()-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(ans[mid]>=x) high = mid-1;
+        else low = mid+1;
+    }
+    ans[high+1] = x;
+}
+int lengthOfLIS(vector<int>& nums) {
+    // vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1,-1));
+    // return lengthOfLIS(nums, dp, 0, -1);
+    vector<int> ans;
+    for(auto it: nums){
+        if(ans.size()){
+            if(it>ans.back()) ans.push_back(it);
+            else find_replacement_and_replace(ans, it);
+        }
+        else ans.push_back(it);
+    }
+    return ans.size();
+}
+
+/*
+    Solution 2: Greedy with Binary Search
+
+    Let's construct the idea from following example.
+    
+    Consider the example nums = [2, 6, 8, 3, 4, 5, 1], let's try to build the increasing subsequences starting with an empty one: sub1 = [].
+    
+    Let pick the first element, sub1 = [2].
+    
+    6 is greater than previous number, sub1 = [2, 6]
+    
+    8 is greater than previous number, sub1 = [2, 6, 8]
+    
+    3 is less than previous number, we can't extend the subsequence sub1, but we must keep 3 because in the future there 
+    may have the longest subsequence start with [2, 3], sub1 = [2, 6, 8], sub2 = [2, 3].
+    
+    With 4, we can't extend sub1, but we can extend sub2, so sub1 = [2, 6, 8], sub2 = [2, 3, 4].
+    
+    With 5, we can't extend sub1, but we can extend sub2, so sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5].
+    
+    With 1, we can't extend neighter sub1 nor sub2, but we need to keep 1, so sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5], sub3 = [1].
+    
+    Finally, length of longest increase subsequence = len(sub2) = 4.
+    
+    In the above steps, we need to keep different sub arrays (sub1, sub2..., subk) which causes poor performance. 
+    But we notice that we can just keep one sub array, when new number x is not greater than the last element of the subsequence sub, we do binary search to find the smallest element >= x in sub, and replace with number x.
+    
+    Let's run that example nums = [2, 6, 8, 3, 4, 5, 1] again:
+    
+    Let pick the first element, sub = [2].
+    
+    6 is greater than previous number, sub = [2, 6]
+    
+    8 is greater than previous number, sub = [2, 6, 8]
+    
+    3 is less than previous number, so we can't extend the subsequence sub. 
+    We need to find the smallest number >= 3 in sub, it's 6. Then we overwrite it, now sub = [2, 3, 8].
+    
+    4 is less than previous number, so we can't extend the subsequence sub. We overwrite 8 by 4, so sub = [2, 3, 4].
+    
+    5 is greater than previous number, sub = [2, 3, 4, 5].
+    
+    1 is less than previous number, so we can't extend the subsequence sub. We overwrite 2 by 1, so sub = [1, 3, 4, 5].
+    
+    Finally, length of longest increase subsequence = len(sub) = 4.
+*/

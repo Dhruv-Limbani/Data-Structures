8612cbc4a12b957245312b9b2d5f434b3462ebc5
Dhruv <informdhruv@gmail.com>
2024-12-27 15:32:45 -0500

    moved

--- recursion_backtracking.cpp/rat_in_maze.cpp
+++ recursion_backtracking.cpp/rat_in_maze.cpp
@@ -3,27 +3,14 @@ using namespace std;
 
 void paths(vector<vector<int>> &mat, vector<string> &ans, string s, int i, int j, int n){
     if(i==n-1 && j==n-1) ans.push_back(s);
+    else if(i<0 || i>=n || j<0 || j>=n) return;
     else if(mat[i][j]){
-        if(i+1<n && mat[i+1][j]==1){
-            mat[i][j] = 0;
-            paths(mat,ans,s+"D",i+1,j,n);
-            mat[i][j] = 1;
-        } 
-        if(j-1>=0 && mat[i][j-1]==1){
-            mat[i][j] = 0;
-            paths(mat,ans,s+"L",i,j-1,n);
-            mat[i][j] = 1;
-        }
-        if(j+1<n && mat[i][j+1]==1){
-            mat[i][j] = 0;
-            paths(mat,ans,s+"R",i,j+1,n);
-            mat[i][j] = 1;
-        }
-        if(i-1>=0 && mat[i-1][j]==1){
-            mat[i][j] = 0;
-            paths(mat,ans,s+"U",i-1,j,n);
-            mat[i][j] = 1;
-        }
+        mat[i][j] = 0;
+        paths(mat,ans,s+"D",i+1,j,n);
+        paths(mat,ans,s+"L",i,j-1,n);
+        paths(mat,ans,s+"R",i,j+1,n);
+        paths(mat,ans,s+"U",i-1,j,n);
+        mat[i][j] = 1;
     }
     
 }
@@ -31,4 +18,4 @@ vector<string> findPath(vector<vector<int>> &mat) {
     vector<string> ans;
     paths(mat, ans, "", 0, 0, mat.size());
     return ans;
-}
+}   

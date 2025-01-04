98f0ad60e12e850f534cc74234a9542f450e65c7
Dhruv <informdhruv@gmail.com>
2025-01-04 17:58:54 -0500

    stack conversions completed

---
+++ 7_Stacks_Queues/2_Conversions/6_postfix_to_prefix.cpp
@@ -0,0 +1,7 @@
+// Read the Postfix expression from left to right
+// If the symbol is an operand, then push it onto the Stack
+// If the symbol is an operator, then pop two operands from the Stack 
+// Create a string by concatenating the two operands and the operator before them. 
+// string = operator + operand2 + operand1 
+// And push the resultant string back to Stack
+// Repeat the above steps until end of Postfix expression.
